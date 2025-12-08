9b58fbd31edd183ac8c226cf639eac1a0c85f32b
graven <shinominecro@gmail.com>
2025-12-08 18:22:02 +0800

    editing

--- QuizQuest/forgot_password.php
+++ QuizQuest/forgot_password.php
@@ -139,7 +139,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["reset_request"])) {
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="teacherscripts.js"></script>
-// wdaz mcyb zofe nbcc
 </body>
 </html>
 
